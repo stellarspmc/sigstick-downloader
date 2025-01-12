748420d392fff5e381b61cde737b26e787fcb7c3
tcfplayz <58693033+tcfplayz@users.noreply.github.com>
2025-01-13 01:58:21 +0800

    1.0 - nasty code

--- sigstick_downloader.py
+++ sigstick_downloader.py
@@ -10,18 +10,17 @@ webContent = response.read().decode('UTF-8')
 html = bs(webContent).prettify()
 
 html = html[html.find('<link as="image" href="/assets/icon/imessage256_icon.png'):]
-html = html[:html.find('<link as="image" href="/assets/img/appstore.svg')]
-html = html.replace('<link as="image" href="', "")
-html = html.replace('/assets/icon/imessage256_icon.png', "")
-html = html.replace('" rel="preload"/>', "")
-html = html.replace('.thumb128', "")
-html = html.replace('  ', "")
-urlList = html.splitlines()
-urlList = [line for line in urlList if line.strip()]
+html = (((((html[:html.find('<link as="image" href="/assets/img/appstore.svg')]
+        .replace('<link as="image" href="', ""))
+        .replace('/assets/icon/imessage256_icon.png', ""))
+        .replace('" rel="preload"/>', ""))
+        .replace('.thumb128', ""))
+        .replace('  ', ""))
+urlList = [line for line in html.splitlines() if line.strip()]
 
-index1 = urlList[0]
-index1 = index1.replace('https://imgcdn.sigstick.com/', "")
-index1 = index1.replace('/0.webp', "")
+index1 = (urlList[0]
+          .replace('https://imgcdn.sigstick.com/', "")
+          .replace('/0.webp', ""))
 
 os.mkdir(folderName)
 for line in urlList:
